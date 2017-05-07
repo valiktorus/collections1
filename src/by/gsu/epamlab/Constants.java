ddbd7f24e995a39bbb8f42ab95462b584be2c3e6
valiktorus <valiktorus@mail.ru>
2017-05-08 01:25:22 +0300

    1

--- src/by/gsu/epamlab/Constants.java
+++ src/by/gsu/epamlab/Constants.java
@@ -1,36 +1,5 @@
 package by.gsu.epamlab;
 
 public class Constants {
-    public static final String TAB = "\t->";
-    //Purchase
-    public static final int ZERO = 0;
-    public static final String EMPTY_LINE = "";
-    //PriceDiscountPurchase
-    public static final int MAX_DISCOUNT = 100;
-    //Exceptions
-    public static final String ERROR_EXCEPTION_DELIMITER = " ";
-    public static final int NAME_INDEX = 0;
-    public static final int PRICE_INDEX = 1;
-    public static final int UNITS_NUMBER_INDEX = 2;
-    public static final int DISCOUNT_INDEX = 3;
     public static final String DELIMITER = ";";
-    public static final String ERROR_EMPTY_NAME = TAB + "empty name";
-    public static final String NON_POSITIVE_VALUE = TAB + "non positive value ";
-    public static final String IN = " in ";
-    // PurchaseList
-    public static final String ERROR_WRONG_NUMBER = TAB + "wrong number of arguments";
-    public static final String ERROR_FORMAT_NUMBER = TAB + "wrong format of arguments";
-    public static final String SRC = "src/";
-    public static final String CSV = ".csv";
-    public static final String TOTAL_PRICE_FORMAT = "%-16s%49s%n";
-    public static final String TOTAL_COST = "Total cost";
-    public static final String NEW_LINE = "\n";
-    public static final String MINUS = "-";
-    public static final String COLLECTION_IS_NOT_SORTED = "Collection is not sorted";
-    public static final int ONE = 1;
-    //Comparators
-    public static final String BY_GSU_EPAMLAB_COMPARATORS = "by.gsu.epamlab.comparators.";
-    public static final int MINUS_ONE = -1;
-    public static final String PURCHASE = "purchase";
 }
-
