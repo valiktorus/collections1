57f54b6d12d81f363d1b9fd58a8f5000f8f88e00
valiktorus <valiktorus@mail.ru>
2017-05-10 21:28:38 +0300

    1

--- src/by/gsu/epamlab/enums/WeekDay.java
+++ src/by/gsu/epamlab/enums/WeekDay.java
@@ -1,6 +1,6 @@
 package by.gsu.epamlab.enums;
 
 public enum WeekDay {
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
 
 }
