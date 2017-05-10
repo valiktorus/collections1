3937f827a150b4bc371b5c9cce6ae830fb33773d
valiktorus <valiktorus@mail.ru>
2017-05-10 21:56:01 +0300

    1

--- src/GeneralConstants.java
+++ src/GeneralConstants.java
@@ -2,12 +2,11 @@ import by.gsu.epamlab.beans.Purchase;
 
 public class GeneralConstants {
     public static final String FILE_NAME = "src/in.csv";
+    public static final String FILE_NOT_FOUND = "File not Found";
+
     public static final String LAST_PURCHASE_MAP = "Last purchase map";
     public static final String FIRST_PURCHASE_MAP = "First purchase map";
     public static final String ENUMERATED_MAP = "Enumerated map ";
-
-    public static final String FILE_NOT_FOUND = "File not Found";
-    public static final String IS_NOT_FOUND = "is not found";
 
     public static final String TOTAL_COST_0 = " Total cost: 0";
     public static final String TOTAL_COST = "Total cost: ";
@@ -17,6 +16,7 @@ public class GeneralConstants {
 
     public static final String PRINT_FORMAT = "%s = %s%n";
     public static final String FIND_VALUE_FORMAT = "Required value %s in %s: ";
+    public static final String IS_NOT_FOUND = "is not found";
 
     public static final Purchase FIRST_BREAD_PURCHASE = new Purchase("bread", 155, 0);
     public static final Purchase SECOND_BREAD_PURCHASE = new Purchase("bread", 170, 0);
