b73fb4da1c87098135bbf9288284d6c1fa47c042
valiktorus <valiktorus@mail.ru>
2017-05-10 19:51:36 +0300

    1

--- src/by/gsu/epamlab/beans/Purchase.java
+++ src/by/gsu/epamlab/beans/Purchase.java
@@ -65,7 +65,9 @@ public class Purchase{
 
         Purchase purchase = (Purchase) o;
 
-        if (!name.equals(purchase.name)) return false;
+        if (!name.equals(purchase.name)) {
+            return false;
+        }
         return price.equals(purchase.price);
     }
 
