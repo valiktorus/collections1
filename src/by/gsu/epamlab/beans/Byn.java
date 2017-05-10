058f4d89c4882c7c9568a1252ed35f58858158f7
valiktorus <valiktorus@mail.ru>
2017-05-10 20:20:19 +0300

    1

--- src/by/gsu/epamlab/beans/Byn.java
+++ src/by/gsu/epamlab/beans/Byn.java
@@ -77,8 +77,12 @@ public class Byn implements Comparable<Byn>{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Byn byn = (Byn) o;
 
